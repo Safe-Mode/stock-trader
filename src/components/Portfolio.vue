942b5f1b33a255ec8538449a99218742bb4ce3a3
Safe_Mode <safe2mode@gmail.com>
2019-02-17 20:55:35 +0300

    Add empty portfolio message

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,8 +1,12 @@
-<template lang="html">
+<template>
   <section class="portfolio">
+    <p class="portfolio__message"
+        v-if="!userStocks.length">You've got no any stocks yet...</p>
+
     <ul class="portfolio__list row">
       <li class="portfolio__item col-6"
-          v-for="stock in userStocks">
+          v-for="(stock, index) in userStocks"
+          :key="index">
           <Stock :stock="stock"
               isUser="true"
               @sell-error="onSellError"></Stock>
@@ -44,5 +48,11 @@
   .portfolio__list {
     padding: 0;
     list-style: none;
+  }
+
+  .portfolio__message {
+    margin-top: 30vh;
+    font-size: 1.6rem;
+    text-align: center;
   }
 </style>
